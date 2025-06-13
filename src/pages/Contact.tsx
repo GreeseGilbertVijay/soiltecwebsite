6589657511a97e14248cabe85c83edf290554865
spincocuttingedge <spincocuttingedge@gmail.com>
2025-06-13 15:26:05 +0530

    new changes

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -202,6 +202,20 @@ const Contact = () => {
         </div>
       </section>
 
+      {/* Google Maps Section */}
+      <section className="w-full h-[300px]">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.987187456456!2d80.2342!3d13.0475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52689a3a3a3a3a%3A0x3a3a3a3a3a3a3a3a!2sVyasar%20St%2C%20T.%20Nagar%2C%20Chennai%2C%20Tamil%20Nadu%20600017!5e0!3m2!1sen!2sin!4v1647881234567!5m2!1sen!2sin"
+          width="100%"
+          height="300"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          title="Samatva Awareness Location - T. Nagar, Chennai"
+        ></iframe>
+      </section>
+
       {/* CTA Section */}
       <section className="py-16 bg-gradient-to-r from-blue-800 to-blue-900 text-white">
         <div className="max-w-4xl mx-auto text-center px-4 sm:px-6 lg:px-8">
